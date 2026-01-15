0bd752d908157a9f8e1ec05ee288cb074b71e145
Gokudoma <stephan_wacker@hotmail.com>
2026-01-15 15:30:48 +0100

    hotfix localhost

--- Quizly/settings.py
+++ Quizly/settings.py
@@ -103,6 +103,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
 ]
 
 # Allow sending cookies (Login)
@@ -114,4 +116,6 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
 ]
